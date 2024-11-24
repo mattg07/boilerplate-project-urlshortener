13d12a3d9f19f8e06932fa0a6b2934ff3eda1d51
mattg07 <matiasmgodoy00@gmail.com>
2024-11-24 19:10:50 +0100

    first and last

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
 const app = express();
-
+const urlparser = require("url");
+const dns = require("dns");
+const { parse } = require("path");
+const client = new MongoClient(process.env.DB_URL);
+const db = client.db("urlshortener");
+const urls = db.collection("urls");
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", function (req, res) {
+  console.log(req.body);
+  const url = req.body.url;
+  const parsedUrl = new URL(url);
+  dns.lookup(parsedUrl.hostname, async (err, address) => {
+    if (!address) {
+      console.log(parsedUrl)
+      res.json({ error: "Invalid Url" });
+    } else {
+      const urlCount = await urls.countDocuments({});
+      const urlDoc = {
+        url,
+        short_url: urlCount,
+      };
+      const result = await urls.insertOne(urlDoc);
+      console.log(result);
+      res.json({
+        original_url: parsedUrl,
+        short_url: urlCount,
+      });
+    }
+  });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:short_url", async(req,res) => {
+const shorturl = req.params.short_url;
+const urlDoc = await urls.findOne({short_url: +shorturl})
+res.redirect(urlDoc.url)
+})
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
